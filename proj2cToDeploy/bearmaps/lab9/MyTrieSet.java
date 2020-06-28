8161496a400ad84bc45970e1b5fe8e396af7ce48
Wyatt <wyatt.wang@outlook.com>
2020-06-29 00:14:49 +0800

    proj2c to deploy
    somehow failed

---
+++ proj2cToDeploy/bearmaps/lab9/MyTrieSet.java
@@ -0,0 +1,110 @@
+package bearmaps.lab9;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+public class MyTrieSet implements TrieSet61B {
+    private Node root;
+
+    private class Node {
+        private boolean isKey;
+        private Map<Character, Node> children; //when there are lots of children, use a map, this is equal to p.left, p,right...
+
+        private Node(boolean isKey) {
+            this.isKey = isKey;
+            children = new HashMap<>();
+        }
+    }
+
+    public MyTrieSet() {
+        this.clear();
+    }
+
+    /** Clears all items out of Trie */
+    @Override
+    public void clear() {
+        root = new Node(false);
+    }
+
+    /** Returns true if the Trie contains KEY, false otherwise */
+    @Override
+    public boolean contains(String key) {
+        if (key == null || key.length() < 1) return false;
+        Node p = root;
+        for (int i = 0; i < key.length(); i++) {
+            char ch = key.charAt(i);
+            if (!p.children.containsKey(ch)) return false;
+            p = p.children.get(ch);
+        }
+        //matching all chars, still need to be isKey
+        return p.isKey;
+    }
+
+    /** Inserts string KEY into Trie */
+    @Override
+    public void add(String key) {
+        if (key == null || key.length() < 1) return;
+        Node p = root;
+        for (int i = 0; i < key.length(); i++) {
+            char ch = key.charAt(i);
+            if (!p.children.containsKey(ch)) {
+                p.children.put(ch, new Node(false));
+            }
+            p = p.children.get(ch); //when there are lots of children, use a map, this is equal to p.left, p,right...
+        }
+        p.isKey = true;
+    }
+
+
+    /** Returns a list of all words that start with PREFIX */
+    @Override
+    public List<String> keysWithPrefix(String prefix) {
+        List<String> keys = new LinkedList<>();
+        if (prefix == null || prefix.length() < 1) return keys;
+
+        //find prefix, similar to contain
+        Node p = root;
+        for (int i = 0; i < prefix.length(); i++) {
+            char ch = prefix.charAt(i);
+            if (!p.children.containsKey(ch)) return keys; //doesn't contain prefix, return empty list
+            p = p.children.get(ch);
+        }
+
+        //now node p points at the last char of prefix, then add all words that starts with prefix
+        for (char ch: p.children.keySet()) {
+            keys = collect(prefix + ch, p.children.get(ch), keys);
+        }
+        return keys;
+    }
+
+    /** Returns a list of all the words after Node p in the trie, doesn't include p */
+    private List<String> collect(Node p) {
+        List<String> keysList = new LinkedList<>();
+        for (char ch: p.children.keySet()) {
+            keysList = collect(ch + "", p.children.get(ch), keysList);
+        }
+        return keysList;
+    }
+
+    /** Returns a list of all the words after Node p with PREFIX str, doesn't include p */
+    private List<String> collect(String str, Node p, List<String> keysList) {
+        if (p.isKey) keysList.add(str);
+        for (char ch: p.children.keySet()) {
+            keysList = collect(str + ch, p.children.get(ch), keysList);
+        }
+        return keysList;
+    }
+
+
+    @Override
+    /** Returns the longest prefix of KEY that exists in the Trie
+     * Not required for Lab 9. If you don't implement this, throw an
+     * UnsupportedOperationException.
+     */
+    public String longestPrefixOf(String key) {
+        throw new UnsupportedOperationException();
+    }
+
+
+}
--- proj2cToDeploy/bearmaps/proj2c/AugmentedStreetMapGraph.java
+++ proj2cToDeploy/bearmaps/proj2c/AugmentedStreetMapGraph.java
@@ -2,8 +2,10 @@ package bearmaps.proj2c;
 
 import bearmaps.hw4.streetmap.Node;
 import bearmaps.hw4.streetmap.StreetMapGraph;
+import bearmaps.lab9.MyTrieSet;
 import bearmaps.proj2ab.KDTree;
 import bearmaps.proj2ab.Point;
+import edu.princeton.cs.algs4.ST;
 import org.apache.commons.math3.geometry.partitioning.utilities.AVLTree;
 
 import java.util.*;
@@ -12,26 +14,45 @@ import java.util.*;
  * An augmented graph that is more powerful that a standard StreetMapGraph.
  * Specifically, it supports the following additional operations:
  *
- *
  * @author Alan Yao, Josh Hug, ________
  */
 public class AugmentedStreetMapGraph extends StreetMapGraph {
     private Map<Point, Long> pointToNode = new HashMap<>();
     private KDTree kd;
+    private MyTrieSet trie = new MyTrieSet();
+    private Map<String, List<Node>> nameToNodes = new HashMap<>();
 
     public AugmentedStreetMapGraph(String dbPath) {
         super(dbPath);
-        // You might find it helpful to uncomment the line below:
+
         List<Node> nodes = this.getNodes();
 
-        //map points to nodes.long
+
+        //create a list of Points for kdTree, and map points to nodes
         List<Point> points = new LinkedList<>();
-        for (Node n: nodes) {
+        for (Node n : nodes) {
             //only consider vertices that have neighbors
             if (!this.neighbors(n.id()).isEmpty()) {
                 Point p = new Point(n.lon(), n.lat());
                 points.add(p);
                 pointToNode.put(p, n.id());
+            }
+            if (n.name() != null) {
+                String cleanName = cleanString(n.name());
+
+                //add to trie set
+                trie.add(cleanName);
+
+                //multiple locations may share the same name
+                if (!nameToNodes.containsKey(cleanName)) {
+                    List<Node> nameNodes = new LinkedList<>(); //name appears the first time
+                    nameNodes.add(n);
+                    nameToNodes.put(cleanName, nameNodes);
+                } else {
+                    List<Node> nodesWithSameName = nameToNodes.get(cleanName); //find locations with the same name, update the List<Node>
+                    nodesWithSameName.add(n);
+                    nameToNodes.put(cleanName, nodesWithSameName);
+                }
             }
         }
         kd = new KDTree(points);
@@ -40,6 +61,7 @@ public class AugmentedStreetMapGraph extends StreetMapGraph {
     /**
      * For Project Part II
      * Returns the vertex closest to the given longitude and latitude.
+     *
      * @param lon The target longitude.
      * @param lat The target latitude.
      * @return The id of the node in the graph closest to the target.
@@ -53,19 +75,29 @@ public class AugmentedStreetMapGraph extends StreetMapGraph {
     /**
      * For Project Part III (gold points)
      * In linear time, collect all the names of OSM locations that prefix-match the query string.
+     *
      * @param prefix Prefix string to be searched for. Could be any case, with our without
      *               punctuation.
      * @return A <code>List</code> of the full names of locations whose cleaned name matches the
      * cleaned <code>prefix</code>.
      */
     public List<String> getLocationsByPrefix(String prefix) {
-        return new LinkedList<>();
+        List<String> toReturn = new LinkedList<>();
+        List<String> cleanNames = trie.keysWithPrefix(prefix);
+        for (String s: cleanNames) {
+            List<Node> nodes = nameToNodes.get(s);
+            for (Node n: nodes) {
+                toReturn.add(n.name());
+            }
+        }
+        return toReturn;
     }
 
     /**
      * For Project Part III (gold points)
      * Collect all locations that match a cleaned <code>locationName</code>, and return
      * information about each node that matches.
+     *
      * @param locationName A full name of a location searched for.
      * @return A list of locations whose cleaned name matches the
      * cleaned <code>locationName</code>, and each location is a map of parameters for the Json
@@ -76,13 +108,25 @@ public class AugmentedStreetMapGraph extends StreetMapGraph {
      * "id" -> Number, The id of the node. <br>
      */
     public List<Map<String, Object>> getLocations(String locationName) {
-        return new LinkedList<>();
+        String cleanLocationName = cleanString(locationName);
+        List<Map<String, Object>> toReturn = new LinkedList<>();
+        List<Node> nodes = nameToNodes.get(cleanLocationName);
+        for (Node n: nodes) {
+            Map<String, Object> map = new HashMap<>();
+            map.put("lat", n.lat());
+            map.put("lon", n.lon());
+            map.put("name", n.name());
+            map.put("id", n.id());
+            toReturn.add(map);
+        }
+        return toReturn;
     }
 
 
     /**
      * Useful for Part III. Do not modify.
      * Helper to process strings into their "cleaned" form, ignoring punctuation and capitalization.
+     *
      * @param s Input string.
      * @return Cleaned string.
      */
